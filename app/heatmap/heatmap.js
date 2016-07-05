7c305c762f3bbbcc3b8267a43b1a01bbba8362fe
tayutaedomo <tayutaedomo@gmail.com>
2016-07-05 15:45:01 +0900

    Refactoring of heat-map trial

--- app/heatmap/heatmap.js
+++ app/heatmap/heatmap.js
@@ -1,49 +1,58 @@
 'use strict';
 
-angular.module('myApp.heatmap', ['ngRoute', 'heatmap'])
+(function(){
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/heatmap', {
-    templateUrl: 'heatmap/heatmap.html',
-    controller: 'HeatmapCtrl as heatmap'
-  });
-}])
+  function RouteConfig ($routeProvider) {
+    $routeProvider.when('/heatmap', {
+      templateUrl: 'heatmap/heatmap.html',
+      controller: 'HeatmapCtrl as heatmap'
+    });
+  }
+  RouteConfig.$inject = ['$routeProvider'];
 
-.controller('HeatmapCtrl', ['$heatmap', function($heatmap) {
-  var that = this;
+  function HeatmapController($heatmap) {
+    var that = this;
 
-  // Refer: https://github.com/pa7/heatmap.js/blob/master/examples/angular-heatmap/index.html
-  function generateRandomData(len) {
-    var max = 100;
-    var min = 1;
-    var maxX = document.body.clientWidth;
-    var maxY = document.body.clientHeight;
-    var data = [];
-    while (len--) {
-      data.push({
-        x: ((Math.random() * maxX) >> 0),
-        y: ((Math.random() * maxY) >> 0),
-        value: ((Math.random() * max + min) >> 0),
-        radius: ((Math.random() * 50 + min) >> 0)
-      });
+    // Refer: https://github.com/pa7/heatmap.js/blob/master/examples/angular-heatmap/index.html
+    function generateRandomData(len) {
+      var max = 100;
+      var min = 1;
+      var maxX = document.body.clientWidth;
+      var maxY = document.body.clientHeight;
+      var data = [];
+      while (len--) {
+        data.push({
+          x: ((Math.random() * maxX) >> 0),
+          y: ((Math.random() * maxY) >> 0),
+          value: ((Math.random() * max + min) >> 0),
+          //radius: ((Math.random() * 50 + min) >> 0)
+        });
+      }
+      return {
+        max: max,
+        min: min,
+        data: data
+      }
     }
-    return {
-      max: max,
-      min: min,
-      data: data
-    }
-  };
 
-  this.heatmapData = generateRandomData(1000);
-  this.heatmapConfig = {
-    width: 256,
-    height: 256,
-    blur: .9,
-    opacity:.5
-  };
+    this.heatmapData = generateRandomData(1000);
+    this.heatmapConfig = {
+      width: 256,
+      height: 256,
+      blur: 0.9,
+      opacity: 0.5,
+      radius: 10
+    };
 
-  this.updateData = function() {
-    that.heatmapData = generateRandomData(1000);
-  };
-}]);
+    this.updateData = function() {
+      that.heatmapData = generateRandomData(1000);
+    };
+  }
+  HeatmapController.$inject = ['$heatmap'];
+
+  angular.module('myApp.heatmap', ['ngRoute', 'heatmap'])
+    .config(RouteConfig)
+    .controller('HeatmapCtrl', HeatmapController);
+
+})();
 
