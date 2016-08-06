2ee5a22e0a4aec0a50d79c79d3913da46462c555
tayutaedomo <tayutaedomo@gmail.com>
2016-08-06 22:28:21 +0900

    Refactoring to concat js files

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -1,14 +1,18 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute'])
+(function(){
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
-    templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
-  });
-}])
+  angular.module('myApp.view2', ['ngRoute'])
 
-.controller('View2Ctrl', [function() {
+    .config(['$routeProvider', function($routeProvider) {
+      $routeProvider.when('/view2', {
+        templateUrl: 'view2/view2.html',
+        controller: 'View2Ctrl'
+      });
+    }])
 
-}]);
+    .controller('View2Ctrl', [function() {
+    }]);
+
+})();
+
--- app/vjs-video/vjs-video.js
+++ app/vjs-video/vjs-video.js
@@ -1,35 +1,37 @@
-'use strict';
+(function(){
 
-angular.module('myApp.vjsVideo', ['ngRoute'])
+  angular.module('myApp.vjsVideo', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/vjs-video', {
-    templateUrl: 'vjs-video/vjs-video.html',
-    controller: 'VjsVideoCtrl'
-  });
-}])
+    .config(['$routeProvider', function($routeProvider) {
+      $routeProvider.when('/vjs-video', {
+        templateUrl: 'vjs-video/vjs-video.html',
+        controller: 'VjsVideoCtrl'
+      });
+    }])
 
-.controller('VjsVideoCtrl', ['$scope', function($scope) {
-  var that = this;
+    .controller('VjsVideoCtrl', ['$scope', function($scope) {
+      var that = this;
 
-  this.player = videojs("example_video_1", {}, function(){
-    // Player (this) is initialized and ready.
-  });
+      this.player = videojs("example_video_1", {}, function(){
+        // Player (this) is initialized and ready.
+      });
 
-  // Refer: http://docs.videojs.com/docs/api/player.html#Methodssrc
-  this.player.src([
-    { type: "video/mp4", src: "//vjs.zencdn.net/v/oceans.mp4" },
-    { type: "video/webm", src: "//vjs.zencdn.net/v/oceans.webm" },
-    { type: "video/ogg", src: "//vjs.zencdn.net/v/oceans.ogv" }
-  ]);
+      // Refer: http://docs.videojs.com/docs/api/player.html#Methodssrc
+      this.player.src([
+        { type: "video/mp4", src: "//vjs.zencdn.net/v/oceans.mp4" },
+        { type: "video/webm", src: "//vjs.zencdn.net/v/oceans.webm" },
+        { type: "video/ogg", src: "//vjs.zencdn.net/v/oceans.ogv" }
+      ]);
 
-  // Refer: http://stackoverflow.com/questions/24607140/angularjs-controller-destructor
-  $scope.$on("$destroy", function() {
-    if (that.player) {
-      // Refer: http://stackoverflow.com/questions/14812319/videojs-unable-to-destroy-and-initialize
-      that.player.dispose();
-      that.player = null;
-    }
-  });
-}]);
+      // Refer: http://stackoverflow.com/questions/24607140/angularjs-controller-destructor
+      $scope.$on("$destroy", function() {
+        if (that.player) {
+          // Refer: http://stackoverflow.com/questions/14812319/videojs-unable-to-destroy-and-initialize
+          that.player.dispose();
+          that.player = null;
+        }
+      });
+    }]);
+
+})();
 
