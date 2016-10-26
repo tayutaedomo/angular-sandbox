30a5b4765d709b277f11993bfb210f1ad25aae47
tayutaedomo <tayutaedomo@gmail.com>
2016-10-26 11:05:28 +0900

    Update ui.router trial

--- app/uirouter/app.js
+++ app/uirouter/app.js
@@ -48,5 +48,45 @@ app.config(function($stateProvider, $urlRouterProvider) {
         });
       }
     })
+    .state('terms', {
+      url: '/terms',
+    });
+});
+
+app.run(function ($rootScope, $modal) {
+  /**
+   * Listen to the `$stateChangeStart` event
+   */
+  $rootScope.$on('$stateChangeStart', function (event, toState) {
+    /**
+     * if the new state is not "terms", then ignore it
+     */
+    if(toState.name !== 'terms') return;
+    /**
+     * Open the modal window
+     */
+    $modal.open({
+      template: [
+        '<div class="modal-content">',
+        '<div class="modal-header">',
+        '<h3 class="modal-title">Regulamin</h3>',
+        '</div>',
+        '<div class="modal-body">',
+        '$1. Give us all your money!',
+        '</div>',
+        '<div class="modal-footer">',
+        '<button class="btn btn-primary" ng-click="$dismiss()">OK</button>',
+        '</div>',
+        '</div>'
+      ].join(''),
+      controller: function($scope){
+        // Do whatever you need here.
+      }
+    });
+    /**
+     * Prevent the transition to the dummy state, stay where you are
+     */
+    event.preventDefault();
+  })
 });
 
