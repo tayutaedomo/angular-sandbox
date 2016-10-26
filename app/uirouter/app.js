9c0477e398053c3d521e27b79adb085c19c04c41
tayutaedomo <tayutaedomo@gmail.com>
2016-10-26 10:45:08 +0900

    Add ui.router trial

---
+++ app/uirouter/app.js
@@ -0,0 +1,52 @@
+//
+// Refer: http://blog.dargiel.net/how-to-open-a-modal-from-any-state-using-ui-router/
+//
+var app = angular.module('plunker', ['ui.router', 'ui.bootstrap']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  /**
+   * Specify a default state
+   */
+  $urlRouterProvider.otherwise('/');
+  /**
+   * define a sample state
+   */
+  $stateProvider
+    .state('index', {
+      url: '/',
+      template: '<h2>Hello world</h2>'
+    })
+    /**
+     * Then define a child state for the modal window
+     */
+    .state('index.terms', {
+      /**
+       * Only the URL is required, you don't need a controller or a template for the state
+       */
+      url: '/terms',
+      /**
+       * Open the modal window when the onEnter event is fired
+       */
+      onEnter: function($modal){
+        $modal.open({
+          template: [
+            '<div class="modal-content">',
+            '<div class="modal-header">',
+            '<h3 class="modal-title">Regulamin</h3>',
+            '</div>',
+            '<div class="modal-body">',
+            '$1. Give us all your money!',
+            '</div>',
+            '<div class="modal-footer">',
+            '<button class="btn btn-primary" ng-click="$dismiss()">OK</button>',
+            '</div>',
+            '</div>'
+          ].join(''),
+          controller: function($scope){
+            // do whatever you need here.
+          }
+        });
+      }
+    })
+});
+
