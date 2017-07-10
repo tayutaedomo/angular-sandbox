25ad37e1ec58a681e0a425396753b9566166fe32
tayutaedomo <tayutaedomo@gmail.com>
2017-07-10 19:34:42 +0900

    Add agile crm create_contract

--- app/agilecrm/agilecrm.js
+++ app/agilecrm/agilecrm.js
@@ -29,6 +29,39 @@
           console.log('doSetEmailWithPageView _agile is empty.');
         }
       }
+
+      this.doCreateContact = function() {
+        if (! $window._agile) {
+          console.log('doCreateContact _agile is empty.');
+          return;
+        }
+
+        var contact = {};
+
+        contact.email = that.email;
+        contact.first_name = 'Test';
+        contact.last_name = 'Contact';
+        contact.company = 'abc corp';
+        contact.title = 'lead';
+        contact.phone = '+1-541-754-3010';
+        contact.website = 'http://www.example.com';
+        var address = { 'city':'new delhi', 'state':'delhi', 'country':'india' };
+        contact.address = JSON.stringify(address);
+        contact.tags = 'tag1, tag2';
+
+        // Custom fields can be added to contact object as
+        contact.status = 'incomplete';
+        contact.custom_id = 'EN001C';
+
+        $window._agile.create_contact(contact, {
+          success: function (data) {
+            console.log('success');
+          },
+          error: function (data) {
+            console.log('error');
+          }
+        });
+      };
     }]);
 
 })();
